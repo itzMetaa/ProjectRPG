e0fd3d97e718c70bc8a50a2498bb6d0aa643fbce
itzMetaa <bajkaipatrik4@gmail.com>
2020-08-17 23:26:21 +0200

    Add project files.

---
+++ ProjectRPG/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectRPG
+{
+    class Enemy
+    {
+        int hp; //életerő
+        int def; //védekezés
+        int atk; //támadás
+        string name; //név
+
+        public Enemy(int hp, int def, int atk, string name)
+        {
+            this.hp = hp;
+            this.def = def;
+            this.atk = atk;
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            string nevAlahuzas = "";
+            for (int i = 0; i < name.Length; i++)
+            {
+                nevAlahuzas += "_";
+            }
+
+            return string.Format("-- {0} --\n" +
+                "___{1}___\n\n" +
+                "HP: {2}\n" +
+                "DEF: {3}\n" +
+                "ATK: {4}\n", name, nevAlahuzas, hp, def, atk);
+        }
+    }
+}
---
+++ ProjectRPG/GameMain.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectRPG
+{
+    internal class GameMain
+    {
+        static Random r = new Random();
+
+        public GameMain()
+        {
+            var enemyList = new List<Enemy>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var enemy = new Enemy(r.Next(5, 51), r.Next(1, 10), r.Next(2, 20), randomNev(r.Next(2, 7)));
+                enemyList.Add(enemy);
+            }
+
+            foreach (var enemy in enemyList)
+                Console.WriteLine(enemy);
+
+            Console.ReadLine();
+        }
+
+        private static string randomNev(int nevHossz)
+        {
+            string nev = "";
+            string maganHangzok = "AEIOU";
+            string massalHangzok = "BCDFGHJKLMNPQRSTVWXYZ";
+
+            string betuVariansANevben = "";
+
+            for (int i = 0; i < nevHossz; i++)
+                betuVariansANevben += Convert.ToString(r.Next(0,2));
+
+            foreach (var karakterHely in betuVariansANevben)
+            {
+                var karakter = karakterHely;
+
+                /*
+                if (karakter > 1 || karakter < 0)
+                    karakter = '0';
+                */
+
+                switch (karakter)
+                {
+                    case '0':
+                        nev += maganHangzok[r.Next(0, maganHangzok.Length)]; break;
+                    case '1':
+                        nev += massalHangzok[r.Next(0, massalHangzok.Length)]; break;
+                    default:
+                        break;
+                }
+            }
+
+            /*
+            nev += Convert.ToString(abc[r.Next(1, abc.Length)]).ToUpper();
+            for (int i = 0; i < n - 1; i++)
+            {
+                nev += Convert.ToString(abc[r.Next(0, abc.Length)]).ToLower();
+            }
+            */
+
+            return nev;
+        }
+    }
+}
